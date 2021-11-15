7b281887d670f5b9d307805705cccaeaa39f4339
kiderkider <90707101+kiderkider@users.noreply.github.com>
2021-11-15 23:36:44 +0100

    uncommenting the code

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ import LC from "./Routes/LC";
 function App() {
   return (
     <Routes>
-      {/* <Route path="/main" element={<Main />} />
-      <Route path="/partner" element={<Partner />} /> */}
+      <Route path="/main" element={<Main />} />
+      <Route path="/partner" element={<Partner />} />
       <Route path="/blog" element={<OurBlog />} />
       {/* <Route path="/blog0" element={<OurBlog0 />} /> */}
-      {/* <Route path="/events" element={<Events />} />
+      <Route path="/events" element={<Events />} />
       <Route path="/offices" element={<Offices />} />
-      <Route path="/lc" element={<LC />} /> */}
+      <Route path="/lc" element={<LC />} />
       {/* <p>Change tester</p> */}
     </Routes>
   );
